2b6a936d478ec6b5056f0ff6b32614b3eb1e96f2
Kateryna Kalinina <katerynakalinina7@gmail.com>
2020-09-21 17:04:28 +0300

    Homework17

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const units = [
-    new Unit('archer', 90, 50),
-    new Unit('infantryman', 80, 70),
-    new Unit('chevalier', 100, 100)
+    new Unit('archer', 80, 50),
+    new Unit('infantryman', 70, 55),
+    new Unit('chevalier', 90, 80)
 ];
 
 const squad = new Squad(units);
